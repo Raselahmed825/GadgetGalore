dca1fbc8e9ec9e62ab147b4898f8cb30e75d9df1
raselahmed825 <raselahmed825@gmail.com>
2017-12-06 01:29:35 +0600

    slider updated

--- app/src/main/java/com/galore/gadget/gadgetgalore/MainActivity.java
+++ app/src/main/java/com/galore/gadget/gadgetgalore/MainActivity.java
@@ -1,13 +1,23 @@
 package com.galore.gadget.gadgetgalore;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewParent;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    CustomSwipeAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        adapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(adapter);
     }
 }
