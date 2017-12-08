99222f6172fa0d53c68afa3bbcb7f5e23fdcedb2
raselahmed825 <raselahmed825@gmail.com>
2017-12-09 02:14:59 +0600

    new codes

--- app/src/main/java/com/galore/gadget/gadgetgalore/MainActivity.java
+++ app/src/main/java/com/galore/gadget/gadgetgalore/MainActivity.java
@@ -1,5 +1,7 @@
 package com.galore.gadget.gadgetgalore;
 
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewPager;
@@ -8,7 +10,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewParent;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Timer;
@@ -16,91 +21,46 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    ViewPager viewPager;
-    CustomSwipeAdapter adapter;
 
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle abdt;
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
 
 
-
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager)findViewById(R.id.view_pager);
-        adapter = new CustomSwipeAdapter(this);
-        viewPager.setAdapter(adapter);
 
-        dl= (DrawerLayout)findViewById(R.id.dl);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
 
-        abdt = new ActionBarDrawerToggle(this,dl,R.string.open,R.string.close);
-        abdt.setDrawerIndicatorEnabled(true);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
 
-        dl.addDrawerListener(abdt);
-        abdt.syncState();
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/OpenSans-BoldItalic.ttf");
+        txtSlogan.setTypeface(face);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        final NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
+            public void onClick(View v) {
 
-                if(id==R.id.myprofile){
-                    Toast.makeText(MainActivity.this,"myprofile",Toast.LENGTH_SHORT).show();
-                }else  if(id==R.id.shop){
-                    Toast.makeText(MainActivity.this,"shop",Toast.LENGTH_SHORT).show();
-                }else  if(id==R.id.location){
-                    Toast.makeText(MainActivity.this,"location",Toast.LENGTH_SHORT).show();
-                }else  if(id==R.id.cart){
-                    Toast.makeText(MainActivity.this,"cart",Toast.LENGTH_SHORT).show();
-                }else if(id==R.id.contact){
-                    Toast.makeText(MainActivity.this,"contact",Toast.LENGTH_SHORT).show();
-                }
-
-
-
-                return true;
             }
         });
 
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new MyTimerTask(),2000, 4000);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-
-        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
-    }
-
-
-
-
-    public class MyTimerTask extends TimerTask{
-
-
-        @Override
-        public void run() {
-
-            MainActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if(viewPager.getCurrentItem()==0){
-                        viewPager.setCurrentItem(1);
-                    }else if(viewPager.getCurrentItem()==1){
-                        viewPager.setCurrentItem(2);
-                    }else if(viewPager.getCurrentItem()==1){
-                        viewPager.setCurrentItem(3);
-                    }else {
-                        viewPager.setCurrentItem(0);
-                    }
-                }
-            });
-        }
-    }
 
 }
+
+
