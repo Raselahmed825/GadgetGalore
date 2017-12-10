18ff5c1e8bc807cdc0df8ae746bbff6ae8e1709b
raselahmed825 <raselahmed825@gmail.com>
2017-12-11 01:36:20 +0600

    Ordering and Submitting

---
+++ app/src/main/java/com/galore/gadget/gadgetgalore/Model/Request.java
@@ -0,0 +1,67 @@
+package com.galore.gadget.gadgetgalore.Model;
+
+import java.util.List;
+
+/**
+ * Created by Elements on 12/11/2017.
+ */
+
+public class Request {
+    private String phone;
+    private String name;
+    private String address;
+    private String total;
+    private List<Order> products; //list of products order
+
+
+    public Request() {
+    }
+
+    public Request(String phone, String name, String address, String total, List<Order> products) {
+        this.phone = phone;
+        this.name = name;
+        this.address = address;
+        this.total = total;
+        this.products = products;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<Order> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Order> products) {
+        this.products = products;
+    }
+}
